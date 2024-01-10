5fbeea4728fe8f6a1e2f9ef7691cf1c0a94cc0a2
MennoRooker <mennorooker@hotmail.com>
2024-01-10 12:51:18 +0100

    Eerste verandering

--- traject.py
+++ traject.py
@@ -2,7 +2,7 @@ class Traject:
     def __init__(self, start, eind):
         self.start = start
         self.eind = eind
-        self.afstand = start - eind
+        self.afstand = abs(start - eind)
 
 if __name__ == "__main__":
     traject = Traject(1, 3)
