e1263e96eeb872935a77e127f99ab09473ab45b7
Ron-Lakeman <ron.j.c.lakeman@gmail.com>
2024-01-10 12:37:35 +0100

    traject class test

---
+++ traject.py
@@ -0,0 +1,17 @@
+class Traject:
+    def __init__(self, start, eind):
+        self.start = start
+        self.eind = eind
+        self.afstand = start - eind
+
+if __name__ == "__main__":
+    traject = Traject(1, 3)
+    print(traject.afstand)
+
+
+
+
+
+
+
+
