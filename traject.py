55cf99480243afc360997d29f481f50a61be44c2
MennoRooker <mennorooker@hotmail.com>
2024-01-11 11:50:24 +0100

    eerste branch push

--- traject.py
+++ traject.py
@@ -1,9 +1,8 @@
 class Traject:
-    def __init__(self, start, eind):
-        self.start = start
-        self.eind = eind
-        self.afstand = abs(start - eind)
+    
+    def __init__(self, station1, station2):
+        self.station1 = station1
+        self.station2 = station2
 
-if __name__ == "__main__":
-    traject = Traject(1, 3)
-    print(traject.afstand)
+    def tijd(self):
+        pass
