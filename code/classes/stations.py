c8fe8a52aeaa1de7ae6fe8c4220569d86ee5cb7e
Ron-Lakeman <ron.j.c.lakeman@gmail.com>
2024-01-15 12:20:37 +0100

    classes aangepast

--- code/classes/stations.py
+++ code/classes/stations.py
@@ -3,37 +3,34 @@ sys.path.append('../')
 from helpers import read_csv_file
 
 class Station:
-    def __init__(self, name, y, x,):
+    def __init__(self, name):
         self.name = name
-        self.y = y
-        self.x = x
         self.connections = {}
 
-    def create_connections(self):
-        connections = read_csv_file('../../data/ConnectiesHolland.csv')
-        for row in connections:
-            station1 = row[0]
-            station2 = row[1]
-            time = row[2]
-            # print(station1, station2, time)
-            if station1 == self.name:
-                self.connections[station2] = time
-            elif station2 == self.name:
-                self.connections[station1] = time
-                    
+    def create_connection(self, other_station, time):
+        self.connections[other_station] = time
+        
 
 if __name__ == "__main__":
     stations = read_csv_file('../../data/StationsHolland.csv')
-    station_objects = []
+    connections = read_csv_file('../../data/ConnectiesHolland.csv')
     
+    station_objects = []
     for row in stations:
         name = row[0]
-        y = row[1]
-        x = row[2]
-
-        station = Station(name, y, x)
-        station.create_connections()
+        station = Station(name)
         station_objects.append(station)
+
+    for row in connections:
+        main_station = row[0]
+        connected_station = row[1]
+        time = row[2]
+
+        for station in station_objects:
+            if station.name == main_station:
+                station.create_connection(connected_station, time)
+            elif station.name == connected_station:
+                station.create_connection(main_station, time)   
     
     for station in station_objects:
         print(f"Station: {station.name}, Connections: {station.connections}")
--- code/classes/traject.py
+++ code/classes/traject.py
@@ -4,26 +4,47 @@ sys.path.append('../')
 from helpers import read_csv_file
 
 class Traject:
-    def __init__(self):
+    def __init__(self, name):
         self.name = name
         self.stations = []
+        # self.time = 0
     
-    def create_traject(self):
-        pass
+    def add_station(self, other_station):
+        self.stations.append(other_station)
 
-        
+    # def get_connection(self, other_station):
+    #     return self.stations.name
+    
+    # def get_time(self, other_station):
+    #     return self.stations[other_station.time]
+
+
 if __name__ == "__main__":
-    stations = read_csv_file('../../data/StationsHolland.csv')
-    station_objects = []
-    
-    for row in stations:
-        name = row[0]
-        y = row[1]
-        x = row[2]
 
-        station = Station(name, y, x)
-        station.create_connections()
-        station_objects.append(station)
+    connections = read_csv_file('../../data/ConnectiesHolland.csv')
     
-    for station in station_objects:
-        print(f"Station: {station.name}, Connections: {station.connections}")
+    test_stations = []
+    station1 = Station('Hoorn')
+    station2 = Station('Alkmaar')
+    test_stations.append(station1)
+    test_stations.append(station2)
+
+    for row in connections:
+        main_station = row[0]
+        connected_station = row[1]
+        time = row[2]
+
+        for station in test_stations:
+            if station1.name == main_station:
+                station1.create_connection(connected_station, time)
+            elif station1.name == connected_station:
+                station1.create_connection(main_station, time)
+
+
+    traject1 = Traject(station1)
+    traject1.add_station(station)
+
+    print(f"{traject1.name}, {traject1.stations}")
+
+
+    
