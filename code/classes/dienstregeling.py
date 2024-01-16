7f2495fdb2c5b06d50fc2033423ededce392dfe5
Ron-Lakeman <ron.j.c.lakeman@gmail.com>
2024-01-16 11:42:19 +0100

    merge conflict

--- code/classes/dienstregeling.py
+++ code/classes/dienstregeling.py
@@ -8,10 +8,13 @@ class Regeling:
     """
     Een verzameling trajecten die samen zo veel mogelijk stations bereikt in zo min 
     mogelijk tijd. Score: K = p*10000 - (T*100 + Min)
-
+    
     Constraint: max 7 trajecten
     """
-    pass
+    def __init__(self, name) -> None:
+        pass
+
+    def a
 
 
 if __name__ == "__main__":
