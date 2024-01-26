2a576941bc3b97d6c337a7e35b99fb31fd6c767d
MennoRooker <mennorooker@hotmail.com>
2024-01-26 12:47:32 +0100

    hist.py command line arguments

--- code/visualisation/hist.py
+++ code/visualisation/hist.py
@@ -1,38 +1,78 @@
 import sys
 sys.path.append('../')
-from helpers import read_csv_file
 sys.path.append('../classes')
-from dienstregeling import Regeling
 sys.path.append('../algorithms')
+
+from helpers import read_csv_file
+
+from dienstregeling import Regeling
+
 from randalg import run_randalg_N_times
-from randalg2 import run_alg_N_times
+# from randalg2 import run_alg_N_times
+
 import matplotlib
 matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 
 
-# Generate scores list (assuming run_randalg_N_times is defined)
-# scores_list = run_randalg_N_times(10000)
-scores_list = run_alg_N_times(1000)
+"""
+Maakt een histogram van de scores van verschillende algoritmes.  
 
-# Fit a normal distribution to the data
-(mu, sigma) = norm.fit(scores_list)
+Usage: 'python3 hist.py (algorithm)' where (algorithm) is one of the following abbreviations:
 
-# Create histogram without normalizing (density=False)
+rd for random
+gr for greedy
+hc for hill climber
+
+Example: 'python3 hist.py rd' 
+"""
+
+# Verzeker het correcte gebruik van de code
+assert len(sys.argv) == 2, """Usage: 'python3 hist.py (algorithm)' "
+where (algorithm) is on of the following abbreviations:
+
+rd for random
+gr for greedy
+hc for hill climber
+
+Example: 'python3 hist.py rd'
+"""
+
+try:
+    # Ask the user for input
+    N = int(input("How many times do you want to run the algorithm? "))
+# Only accept integers
+except ValueError:
+    print("Please enter a valid integer.")
+
+if sys.argv[1].lower() == 'rd':
+    scores_list = run_randalg_N_times(N)
+    algorithm = 'Random'
+    algorithm_abrev = 'rd'
+elif sys.argv[1].lower() == 'gr':
+    # scores_list = run_alg_N_times(1000)
+    algorithm = 'Greedy'
+    algorithm_abrev = 'gr'
+elif sys.argv[1] == 'hc':
+    # scores_list = run_alg_N_times(1000)
+    algorithm = 'Hill Climber'
+    algorithm_abrev = 'hc'
+else:
+    raise AssertionError ("Geen valide naam!")
+
+
+# Creëer een niet-genormaliseerde histogram (density=False)
 n, bins, patches = plt.hist(scores_list, bins=60, density=False, facecolor='green', alpha=0.75)
 
-# Calculate bin width for scaling the normal distribution curve
-bin_width = bins[1] - bins[0]
-
-# Add a 'best fit' line using the scaled normal distribution curve
-# plt.plot(bins, norm.pdf(bins, mu, sigma) * len(scores_list) * bin_width, 'r--', linewidth=2)
-
-# Add labels and title
+# Voeg labels en titel toe
 plt.xlabel('K')
 plt.ylabel('Frequency')
 # plt.title(r'$\mathrm{Histogram\ of\ scores:}\ \mu=%.3f,\ \sigma=%.3f$' % (mu, sigma))
-plt.title(r'$\mathrm{Histogram\ of\ scores:}$')
+plt.title(f"Scores of {algorithm} Algorithm:")
 plt.grid(True)
+bbox_props = dict(boxstyle="round,pad=0.3", edgecolor="black", facecolor="white")
+plt.text(0.7, 0.95, f"{N} iterations", transform=plt.gca().transAxes, fontsize=11, verticalalignment='top', bbox=bbox_props)
 
+plt.savefig(f"../../docs/{algorithm_abrev}_hist.png")
 plt.show()
