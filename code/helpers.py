4333f3d56f1ef6911ed1893b87434c32f1a0b81a
MennoRooker <mennorooker@hotmail.com>
2024-01-26 13:59:27 +0100

    helpers aangepast

--- code/helpers.py
+++ code/helpers.py
@@ -1,16 +1,46 @@
 import csv
 import json
 
+
+# Leest csv bestanden
 def read_csv_file(filename) -> None:
     rows = []
     with open(filename) as file:
         stations = csv.reader(file)
-        header = next(stations)
+        header = next(stations) # Negeer de boevneste regel van het csv bestand
         for row in stations:
             rows.append(row)
     return rows
 
+
+# Leest GeoJSON bestanden voor de visualisatie
 def read_GeoJSON_file(filename):
     with open(filename, 'r') as file:
         data = json.load(file)
     return data
+
+
+# Berekent de kwaliteitsscore van de gehele dienstregeling
+def calculate_score(traject_list):
+    unique_connections = set()
+    traject_counter = 0
+    minutes = 0
+
+    # Houd de variabelen bij
+    for traject in traject_list:
+        traject_counter += 1
+        minutes += traject.time
+        stations = traject.stations_in_traject
+
+        for i in range(len(stations) - 1):
+            connection = frozenset([stations[i].get_name(), stations[i+1].get_name()])
+            unique_connections.add(connection)
+
+    # Stel variabelen in
+    p = len(unique_connections) / 28
+    T = traject_counter
+    Min = minutes
+
+    # Bereken de score
+    K = round(p * 10000 - (T * 100 + Min))     
+    return K
