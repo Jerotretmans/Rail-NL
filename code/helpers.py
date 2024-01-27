98d9e1182005af5f1641fce5d7badba6365bb827
MennoRooker <mennorooker@hotmail.com>
2024-01-27 15:04:59 +0100

    joe

--- code/helpers.py
+++ code/helpers.py
@@ -1,18 +1,26 @@
 import csv
 import json
+from typing import Union
 
 # Leest csv bestanden
 def read_csv_file(filename) -> None:
     rows = []
     with open(filename) as file:
         stations = csv.reader(file)
-        header = next(stations) # Negeer de boevneste regel van het csv bestand
+        _ = next(stations) # Negeer de boevneste regel van het csv bestand
         for row in stations:
             rows.append(row)
     return rows
 
 # Leest GeoJSON bestanden voor de visualisatie
-def read_GeoJSON_file(filename):
+def read_GeoJSON_file(filename: str) -> Union[dict, None]:
     with open(filename, 'r') as file:
         data = json.load(file)
     return data
+
+def load_algorithms_dict() -> dict:
+    return {'rd': 'random',\
+            'gr': 'greedy',\
+            'hc': 'hill climber',\
+            'df': 'depth first',\
+            'bf': 'breadth first'}
